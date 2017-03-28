08f2cbb9bf8bf02b9a4f58499211bd90c4014b52
tcarrio <tom@carrio.me>
2017-03-27 20:48:10 -0400

    Updated motor loops

--- Motors_nStuff.py
+++ Motors_nStuff.py
@@ -10,22 +10,25 @@ lengthTotal = int(input('Input total required rod length in inches [in]: '))
 lengthRodEnd = int(input('Input required distance between rod end and bearing center in inches [in]: '))
 led = LED(5)
 #Define both distance sensors
-sensor1 = DistanceSensor(echo=12, trigger=23)
-sensor2 = DistanceSensor(echo= , trigger= )
-while True:
-    distanceRead1 = sensor1.distance * 100 / 2.54   #Set distance to a variable and convert to inches from mm
-    distanceRead2 = sensor2.distance * 100 / 2.54
-    print('Distance Sensor 1: ', distanceRead1)
-    print('Distance Sensor 2: ', distanceRead2)
+sensor1 = DistanceSensor(echo=12, trigger=13)
+sensor2 = DistanceSensor(echo=16, trigger=19)
 
-    if(distanceRead1 > lengthRodEnd):
-        led.on() #Motor 1 should be running
-    else:
-        led.off() #Should shut down motor 1 - lengthRodEnd is achieved on one end
-    if(distanceRead1 + distanceRead2 > lengthTotal):
-        led.on() #This is where motor 2 needs to be activated
-    else:
-        led.off() #Should shut down motor 2 -  lengthTotal is achieved
+# MOTOR ONE
+distanceRead1 = sensor1.distance * 100 / 2.54
+while(distanceRead1 > lengthRodEnd):
+    led.on() #Motor 1 should be running
+    print('Distance Sensor 1: ', distanceRead1)
+    distanceRead1 = sensor1.distance * 100 / 2.54
+    #Set distance to a variable and convert to inches from mm
+led.off()
+
+# MOTOR TWO
+distanceRead2 = sensor2.distance * 100 / 2.54
+while(distanceRead1 + distanceRead2 > lengthTotal):
+    led.on() #This is where motor 2 needs to be activated
+    print('Distance Sensor 2: ', distanceRead2)
+    distanceRead2 = sensor2.distance * 100 / 2.54
+led.off() #Should shut down motor 2 -  lengthTotal is achieved
 
 
 
